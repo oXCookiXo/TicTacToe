b9e8b5844eb19e07ca6d88a36a6ffca48a540fb9
Carlos <oxcookixo@gmail.com>
2016-04-23 00:25:05 -0600

    Added files via upload

---
+++ WpfApplication3/WpfApplication3/User.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication3
+{
+    public class User
+    {
+
+        private string username;
+        private int score;
+        private bool guestPlayer;
+
+        //Constructor use when it is a guest player
+        public User()
+        {
+            username = "Guest";
+            guestPlayer = true;
+            score = 0;
+        }
+
+        //Constructor use when it is a login player
+        public User(string userData)
+        {
+            username = userData;
+            guestPlayer = false;
+            score = 0;
+        }
+
+        //Copy Constructor
+        public User(User prevUser)
+        {
+            username = prevUser.username;
+            score = 0;
+            guestPlayer = prevUser.guestPlayer;
+        }
+
+        //Desctructor
+        ~User()
+        {
+            username = "";
+            score = 0;
+            guestPlayer = false;
+        }
+
+        public void setUsername(string userData)
+        {
+            username = userData;
+        }
+
+        public string getUsername()
+        {
+            return username;
+        }
+
+        public void setGuestPlayer(bool isGuest)
+        {
+            guestPlayer = isGuest;
+        }
+
+        public bool isGuest()
+        {
+            return guestPlayer;
+        }
+
+        public void addScore(){
+            score++;
+        }
+
+        public int getScore()
+        {
+            return score;
+        }
+    }
+}
